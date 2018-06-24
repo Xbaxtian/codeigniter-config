c679e4e63f2f5236dcb37e6601c15943f44d2442
xbaxtian <xbaxtian@hotmail.com>
2018-06-24 18:59:08 -0500

    index.php removido y htacces añadido

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
-
+if($_SERVER == 'localhost')
+	$config['base_url'] = 'http://localhost/codeigniter-config/';
+else
+	$config['base_url'] = '';
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -35,7 +37,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
